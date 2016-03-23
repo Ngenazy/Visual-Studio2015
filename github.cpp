1bd2725776756ff12041a5c4f5bcd722c8a92bed
Mukudzei M Mangena <badeovolente@gmail.com>
2016-03-23 19:22:26 +0200

    Linking VS2015 to gitHub

---
+++ github.cpp
@@ -0,0 +1,10 @@
+//Authour: Mukudzei Makepeace Mangena
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cout << "We are trying to upload this file to gitHub, then clone it an commit before merging"<<"\n" << endl;
+	return 0;
+}
